42a840b975a8916b29b41edc3fbd5dddd25fd151
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-24 10:06:07 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname)));
 
